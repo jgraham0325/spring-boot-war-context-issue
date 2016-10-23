1d0d4a9e05504753d1817845a299ea18ded1eedb
jgraham0325 <jgraham0325@gmail.com>
2016-10-23 19:21:20 +0100

    added jsp page with context. Can be accessed from http://localhost:8083/maintest on local or http://localhost:8083/spring-boot-war-context-issue/maintest. Working locally but when on external Tomcat, throwing error: boot.web.support.ErrorPageFilter     : Cannot forward to error page for request [/maintest] as the response has already been committed. As a result, the response may have the wrong status code.

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -1,14 +1,39 @@
 package hello;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController
+@Controller
 public class HelloController {
+	
+	@SuppressWarnings("unused")
+	private final Logger logger = LoggerFactory
+			.getLogger(HelloController.class);
+	
+	//example of loading a property value from application.properties into a Java file
+	@Value("${propValue1}")
+	private String propValue;
     
     @RequestMapping("/endpoint")
-    public String index() {
-        return "Endpoint is working...";
+    public String index() throws Exception {
+    	
+    	if (true){
+    		
+    		logger.error("TEST ERROR MESSAGE 1", new Exception("exception123"));
+    	}
+    	
+        return "Endpoint is working..."+propValue;
     }
+    
+    @GetMapping("/maintest")
+	public String main(Model model) {
+		return "main";
+	}
     
 }
---
+++ src/main/resources/META-INF/resources/WEB-INF/jsp/main.jsp
@@ -0,0 +1,14 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1" pageEncoding="ISO-8859-1"%>
+    <html>
+	<head>
+	<c:set var="contextPath" value="${pageContext.request.contextPath}"/>
+	<link rel="stylesheet" type="text/css" href="${contextPath}/app/styles/style.css">
+	</head>
+	
+	<body>
+	Test static HTML page
+	
+	<img src="${contextPath}/app/images/springboot.png" />/>
+	</body>
+	
+</html>
